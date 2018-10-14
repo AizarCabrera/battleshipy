f876331eff6d4a8bc878b58e5dc6c24375f8d4f9
Aizar <aizar.cabrera@icloud.com>
2018-10-14 13:47:02 -0600

    makes cells for board

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,26 @@
 class Board
 
+attr_reader :cells
+
+def initialize
+  @cells = [
+    Cell.new('A', '1'),
+    Cell.new('A', '2'),
+    Cell.new('A', '3'),
+    Cell.new('A', '4'),
+    Cell.new('B', '1'),
+    Cell.new('B', '2'),
+    Cell.new('B', '3'),
+    Cell.new('B', '4'),
+    Cell.new('C', '1'),
+    Cell.new('C', '2'),
+    Cell.new('C', '3'),
+    Cell.new('C', '4'),
+    Cell.new('D', '1'),
+    Cell.new('D', '2'),
+    Cell.new('D', '3'),
+    Cell.new('D', '4'),
+  ]
+end
 
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper'
 require './lib/board'
+require './lib/cell'
 
 class BoardTest < Minitest::Test
   def setup
@@ -10,8 +11,10 @@ class BoardTest < Minitest::Test
     assert_instance_of Board, @board
   end
 
-  def test_it_has_one_cell
-    assert_instance_of Cell, @board.cell
+  def test_it_can_have_16_cells
+    assert_instance_of Array, @board.cells
+    assert_equal 16, @board.cells.length
   end
+
 
 end
